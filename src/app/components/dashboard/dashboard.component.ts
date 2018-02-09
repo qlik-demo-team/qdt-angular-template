60f7784db722e8f5da8af7f9e12e87987a6d70b7
Yianni Ververis <yiannisver@gmail.com>
2018-02-09 15:04:19 -0500

    support latest qdt-components v1.1.1

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -8,15 +8,72 @@ import { HighlightJsService } from 'angular2-highlight-js';
 })
 export class DashboardComponent implements OnInit, AfterViewInit {
 
-    constructor(private el: ElementRef, private service : HighlightJsService) {}
+  constructor(private el: ElementRef, private service : HighlightJsService) {}
+
+  btn1props = {title:'Clear Selections', type:'clearSelections'};
   viz1props = {id: 'a5e0f12c-38f5-4da9-8f3f-0e4566b28398', height:'300px'};
   viz2props = {id: '298bbd6d-f23d-4469-94a2-df243d680e0c', height:'300px'};
-  jsContent1="viz1props = {id: 'a5e0f12c-38f5-4da9-8f3f-0e4566b28398', height:'300px'}";
-  jsContent2="viz1props = {id: 'a5e0f12c-38f5-4da9-8f3f-0e4566b28398', height:'300px'}";
+  viz3props = {id: 'ycppXj', height:'200px'};
+//   kpi1props = {qMeasure: `Count( {$<Priority={'High'}, Status -={'Closed'} >} Distinct %CaseId )`, listenToSelections: false};
+  kpi1props = {cols: [null,`=Count( {$<Priority={'High'}, Status -={'Closed'} >} Distinct %CaseId )`]};
+  kpi2props = {cols: [null,`=Count( {$<Priority={'Medium'}, Status -={'Closed'} >} Distinct %CaseId )`]};
+  kpi3props = {cols: [null,`=Count( {$<Priority={'Low'}, Status -={'Closed'} >} Distinct %CaseId )`]};
+
+  htmlContent = `
+    <div class="row">
+        <div class="col-md-12 text-right">
+            <qdt-component [Component]="'QdtButton'" [props]="btn1props"></qdt-component>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <qdt-component [Component]="'QdtSelectionToolbar'"></qdt-component>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <qdt-component [Component]="'QdtViz'" [props]="viz3props"></qdt-component>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-4 text-center">
+            <h4>High Priority Cases</h4>
+            <h2><qdt-component [Component]="'QdtKpi'" [props]="kpi1props"></qdt-component></h2>
+        </div>
+        <div class="col-md-4 text-center">
+            <h4>Medium Priority Cases</h4>
+            <h2><qdt-component [Component]="'QdtKpi'" [props]="kpi2props"></qdt-component></h2>
+        </div>
+        <div class="col-md-4 text-center">
+            <h4>Low Priority Cases</h4>
+            <h2><qdt-component [Component]="'QdtKpi'" [props]="kpi3props"></qdt-component></h2>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <qdt-component [Component]="'QdtViz'" [props]="viz1props"></qdt-component>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <qdt-component [Component]="'QdtViz'" [props]="viz2props"></qdt-component>
+        </div>
+    </div>`;
+  jsContent=`
+    btn1props = {title:'Clear Selections', type:'clearSelections'};
+    viz1props = {id: 'a5e0f12c-38f5-4da9-8f3f-0e4566b28398', height:'300px'};
+    viz2props = {id: '298bbd6d-f23d-4469-94a2-df243d680e0c', height:'300px'};
+    viz3props = {id: 'ycppXj', height:'200px'};
+    kpi1props = {qMeasure: "Count( {$<Priority={'High'}, Status -={'Closed'} >} Distinct %CaseId )", listenToSelections: true};
+    kpi2props = {qMeasure: "Count( {$<Priority={'Medium'}, Status -={'Closed'} >} Distinct %CaseId )", listenToSelections: true};
+    kpi3props = {qMeasure: "Count( {$<Priority={'Low'}, Status -={'Closed'} >} Distinct %CaseId )", listenToSelections: true};
+  
+  `;
 
   ngOnInit() {
   }
   ngAfterViewInit() {        
-        this.service.highlight(this.el.nativeElement.querySelector('.typescript'));
-    }
+    this.service.highlight(this.el.nativeElement.querySelector('.typescript'));
+    this.service.highlight(this.el.nativeElement.querySelector('.html'));
+  }
 }
--- src/app/components/qdt-component/qdt-component.component.ts
+++ src/app/components/qdt-component/qdt-component.component.ts
@@ -1,18 +1,18 @@
 import { Component, OnInit, ElementRef, Input } from '@angular/core';
 // import * as qConfig from '../../../qConfig.json';
-import QdtComponents from 'qdt-components';
+import * as QdtComponents from 'qdt-components';
 let qConfig = {
-"config": {
-    "host": "sense-demo.qlik.com",
-    "secure": true,
-    "port": 443,
-    "prefix": "/",
-    "appId": "133dab5d-8f56-4d40-b3e0-a6b401391bde"
-},
-"connections": { 
-    "vizApi": true, 
-    "engineApi": false 
-}
+    "config": {
+        "host": "sense-demo.qlik.com",
+        "secure": true,
+        "port": 443,
+        "prefix": "/",
+        "appId": "133dab5d-8f56-4d40-b3e0-a6b401391bde"
+    },
+    "connections": { 
+        "vizApi": true, 
+        "engineApi": true 
+    }
 }
 
 @Component({
@@ -22,15 +22,15 @@ let qConfig = {
 })
 export class QdtComponentComponent implements OnInit {
 
-	@Input() Component: Function;
+	@Input() Component: string;
     @Input() props: object;
-    
+
     static QdtComponent = new QdtComponents(qConfig.config, qConfig.connections);
 
 	constructor(private elementRef: ElementRef) { }
 
 	ngOnInit() {
-        QdtComponentComponent.QdtComponent.render('QdtViz', this.props, this.elementRef.nativeElement);
+        QdtComponentComponent.QdtComponent.render(this.Component, this.props, this.elementRef.nativeElement);
 	}
 
 }
