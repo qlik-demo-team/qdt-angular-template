e78cdbd80c85e5ee9a512430a47fdf0d2b0b097b
yiannisver <yiannisver@gmail.com>
2020-06-25 19:07:17 -0400

    testing

--- src/app/components/embed-object/embed-object.component.ts
+++ src/app/components/embed-object/embed-object.component.ts
@@ -11,51 +11,16 @@ export class EmbedObjectComponent implements OnInit {
 	constructor(private el: ElementRef, private service : HighlightJsService) { }
 
 	viz1 = {
-		type: 'QdtSelectionToolbar',
 		props: {
-			type: 'QdtSelectionToolbar', height: '300px',
-		},
-	};
-	viz2 = {
-		type: 'QdtViz',
-		props: {
-      type: 'barchart',
-      id: 'a5e0f12c-38f5-4da9-8f3f-0e4566b28398',
-      height: '300px',
-      exportData: true,
-      exportImg: true,
-      exportImgOptions: { width: 600, height: 400, format: 'JPG' },
-      exportPdf: true,
-      exportPdfOptions: { documentSize: { width: 300, height: 150 } },
+            id: 'a5e0f12c-38f5-4da9-8f3f-0e4566b28398',
+            height: 400,
 		},
     };
     
     htmlContent = `
-        <div class="row">
-            <div class="col-md-12">
-                <qdt-component [Component]="viz1.type" [props]="viz1.props"></qdt-component>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-12">
-                <qdt-component [Component]="viz2.type" [props]="viz2.props"></qdt-component>
-            </div>
-        </div>        
     `;
 
     jsContent = `
-        viz1 = {
-            type: 'QdtSelectionToolbar',
-            props: {
-                type: 'QdtSelectionToolbar', height: '300px',
-            },
-        };
-        viz2 = {
-            type: 'QdtViz',
-            props: {
-                type: 'barchart', id: 'a5e0f12c-38f5-4da9-8f3f-0e4566b28398', height: '300px',
-            },
-        };
     `;
 
 	ngOnInit() {
--- src/app/components/qdt-component/qdt-component.component.ts
+++ src/app/components/qdt-component/qdt-component.component.ts
@@ -1,18 +1,13 @@
 import { Component, OnInit, ElementRef, Input } from '@angular/core';
-import QdtComponents from 'qdt-components';
+import { qdtEnigma, qdtCapabilityApp, qdtCompose, QdtViz } from 'qdt-components';
 
-const qConfig = {
-    "config": {
-        "host": "sense-demo.qlik.com",
-        "secure": true,
-        "port": 443,
-        "prefix": "",
-        "appId": "133dab5d-8f56-4d40-b3e0-a6b401391bde"
-    },
-    "connections": { 
-        "vizApi": true, 
-        "engineApi": true 
-    }
+const config = {
+    "host": "sense-demo.qlik.com",
+    "secure": true,
+    "port": 443,
+    "prefix": "",
+    "identity": "yianni",
+    "appId": "133dab5d-8f56-4d40-b3e0-a6b401391bde"
 }
 
 
@@ -24,14 +19,37 @@ const qConfig = {
 export class QdtComponentComponent implements OnInit {
 
 	@Input() Component: string;
-  @Input() props: object;
+  @Input() properties: object;
+  @Input() options: object;
+  @Input() connection: number;
 
-  static qdtComponent = new QdtComponents(qConfig.config, qConfig.connections);
+  // static { qdtCapabilityApp } = QdtComponents;
+  // static qdtComponent = new QdtComponents(qConfig.config, qConfig.connections);
+  static capabilityApp = qdtCapabilityApp(config);
+  static engineApp = qdtEnigma(config);
     
 	constructor(private elementRef: ElementRef) { }
 
   ngOnInit() {
-    QdtComponentComponent.qdtComponent.render(this.Component, this.props, this.elementRef.nativeElement);
+    // QdtComponentComponent.qdtComponent.render(this.Component, this.props, this.elementRef.nativeElement);
+    if (this.connection === 1) {
+      QdtViz({
+        element: this.elementRef.nativeElement,
+        app: QdtComponentComponent.capabilityApp,
+        options: {
+          id: 'a5e0f12c-38f5-4da9-8f3f-0e4566b28398',
+          height: 400,
+        },
+      });
+    } else {
+      qdtCompose({
+        element: this.elementRef.nativeElement,
+        component: this.Component,
+        app: QdtComponentComponent.engineApp,
+        propertie: this.properties,
+        options: this.options,
+      });
+    }
   }
 
 }
